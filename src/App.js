7eea4b79453d575cdb7363f01cc842eb0b26d2ef
ken1lnw <moowinza@gmail.com>
2023-11-18 13:53:40 +0700

    r01 11 23

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ function App() {
       <Layout />
 
 
+
+
+
+
     </>
   );
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,14 +3,20 @@ import logo from '../logo.svg';
 import Navbar from './Navbar';
 
 import TableStudent from './TableStudent';
+import R01 from './R01';
+import R11 from './R11';
+import R23 from './R23';
 
 
 
 function Layout() {
     return (
         <>
-        <Navbar/>
-        <TableStudent/>
+            <Navbar />
+            {/* <TableStudent /> */}
+            {/* <R01 /> */}
+            <R11 />
+            {/* <R23/> */}
         </>
     );
 }
