827b4c015c9152080746477db2b4ec62be1f6d80
ken1lnw <moowinza@gmail.com>
2023-11-20 12:43:17 +0700

    Login

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,6 +8,7 @@ import R11 from './R11';
 import R23 from './R23';
 import R16 from './R16';
 import R17 from './R17';
+import Login from './Login';
 
 
 
@@ -21,6 +22,8 @@ function Layout() {
             <R23/>
             <R16/>
             <R17/>
+
+            <Login />
         </>
     );
 }
